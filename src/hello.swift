692e01fc6df4e1b9566dbffd920cc21294ea66e9
arasan01 <developer@arasan01.com>
2024-05-05 03:09:32 +0900

    いったんOK

--- src/hello.swift
+++ src/hello.swift
@@ -1,6 +1,10 @@
 import Foundation
+import CxxStdlib
 import MUNIMUNI
 
 public func hello() {
-
+  let value = OBS_ICON_TYPE_AUDIO_INPUT.rawValue
+  let logStr = "obs icon audio input value: \(value)"
+  let cxxLogStr = std.string(logStr)
+  swift_log_obs_print(cxxLogStr)
 }
